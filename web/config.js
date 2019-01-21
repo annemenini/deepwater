4c1e3398ccdd9b0e9881b0d52f77fb3d718768fc
Steren Giannini <steren.giannini@gmail.com>
2019-01-21 04:33:21 +0000

    Web refactor to process example or input

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 const baseURL = 'https://storage.googleapis.com/deepwater/model';
-const version = 'v4';
+const version = 'v5';
 
 export const modelURL = `${baseURL}/${version}/tensorflowjs_model.pb`
 export const weightsURL = `${baseURL}/${version}/weights_manifest.json`
--- web/index.js
+++ web/index.js
@@ -6,6 +6,19 @@ import {modelURL, weightsURL} from './config.js'
 const INPUT_NODE_NAME = 'degraded';
 const OUTPUT_NODE_NAME = 'final_output';
 
+let model;
+
+async function loadModel() {
+  if(!model) {
+    const loadingEl = document.getElementById('loading'); 
+    loadingEl.style.display = 'block';
+    console.log('model: loading');
+    model = await loadFrozenModel(modelURL, weightsURL);
+    console.log('model: loaded');
+    loadingEl.style.display = 'none';
+  }
+};
+
 function handleFiles(files) {
   for (let i = 0; i < files.length; i++) {
     let file = files[i];
@@ -13,20 +26,21 @@ function handleFiles(files) {
     if (!file.type.startsWith('image/')){ continue }
     
     let reader = new FileReader();
-    reader.onload =  e => { processImage(e.target.result); };
+    reader.onload =  e => { processFile(e.target.result); };
     reader.readAsDataURL(file);
   }
 }
 
-function processImage(url) {
+function processFile(url) {
   let img = document.createElement("img");
   img.onload = function() {
-    drawImage(this);
+    processImg(this);
   }
   img.src = url;
 }
 
 function drawImage(img) {
+  console.log("drawing input");
   let output = document.getElementById("output");
 
   let canvas = document.createElement("canvas");
@@ -38,53 +52,58 @@ function drawImage(img) {
   output.append(canvas);
 }
 
-async function loadModel() { 
-  console.log('model: loading');
-  const model = await loadFrozenModel(modelURL, weightsURL);
-  console.log('model: loaded');
-
-  document.getElementById('loading').style.display = 'none';
-  document.getElementById('main').style.display = 'block';
-
-  // TODO apply to all input
-  //const input = document.querySelector(".input-preview");
-  const demo = document.getElementById('demo');
-  const demoFloat32 = tf.fromPixels(demo).asType('float32');
-  console.log('model: executing');
-  let result = model.execute({[INPUT_NODE_NAME]: demoFloat32}, OUTPUT_NODE_NAME);
-  // result is a 4
-  console.log('model: executed');
-
-  let output = document.getElementById("demos");
-  let canvas = document.createElement("canvas");
-  canvas.width = demo.naturalWidth;
-  canvas.height = demo.naturalHeight;
+function drawResult(originalImage, result) {
+  console.log("drawing result");
+  const output = document.getElementById("output");
+  const canvas = document.createElement("canvas");
+  canvas.className = "result-preview";
+  canvas.width = originalImage.naturalWidth;
+  canvas.height = originalImage.naturalHeight;
 
   tf.toPixels(result.asType('int32').squeeze(), canvas);
-  output.append(canvas);
 
-  console.log("now what?");
+  output.append(canvas);
+}
+
+function processImg(img) {
+  drawImage(img);
+
+  const demoFloat32 = tf.fromPixels(img).asType('float32');
+  console.log('model: executing');
+  let result = model.execute({[INPUT_NODE_NAME]: demoFloat32}, OUTPUT_NODE_NAME);
+  console.log('model: executed');
+
+  drawResult(img, result);
 }
 
 export async function bindPage() {
-  let inputElement = document.getElementById("input");
-  inputElement.addEventListener("change", function() {handleFiles(this.files)}, false);
+  const loadButton = document.getElementById("run-example");
+  loadButton.addEventListener("click", async () => {
+    await loadModel();
+    processImg(document.getElementById('demo'));
+  }, false);
 
-  let dropzone = document.getElementById("dropbox");
+  const inputElement = document.getElementById("input");
+  inputElement.addEventListener("change", async (event) => {
+    await loadModel();
+    handleFiles(event.target.files);
+  }, false);
 
-  dropzone.ondragover = dropzone.ondragenter = function(event) {
+  const dropzone = document.getElementById("dropbox");
+
+  dropzone.ondragover = dropzone.ondragenter = (event) => {
     event.stopPropagation();
     event.preventDefault();
   }
-  dropzone.ondrop = function drop(e) {
+  dropzone.ondrop = async (e) => {
     e.stopPropagation();
     e.preventDefault();
     var dt = e.dataTransfer;
     var files = dt.files;
   
+    await loadModel();
     handleFiles(files);
   }
 }
 
 bindPage();
-loadModel();
