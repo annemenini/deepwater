302cf3505a121084d69c1557859e69c0a3b4b7ba
Steren Giannini <steren.giannini@gmail.com>
2019-01-21 18:17:32 +0000

    use model v6

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 const baseURL = 'https://storage.googleapis.com/deepwater/model';
-const version = 'v5';
+const version = 'v6';
 
 export const modelURL = `${baseURL}/${version}/tensorflowjs_model.pb`
 export const weightsURL = `${baseURL}/${version}/weights_manifest.json`
